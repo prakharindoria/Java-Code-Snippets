a51d51ec1c3de16f22639df5cd65786bfe54e1f9
Prakhar Indoria <prakharindoria@gmail.com>
2020-07-25 11:09:07 +0530

    Update DayFromDate.java

--- DayFromDate.java
+++ DayFromDate.java
@@ -1,5 +1,5 @@
 class Result {
-    public static String findDay(int m, int d, int y) {
+    public static String findDay(int d, int m, int y) {
         return LocalDate.of(y,m,d).getDayOfWeek().name();
     }
 
@@ -7,7 +7,7 @@ class Result {
 class MainClass{
 public static void main(String []args){
 Scanner kb=new Scanner(System.in);
-System.out.prinln("Enter Date in Format: dd/mm/yyyy");
+System.out.println("Enter Date in Format: dd/mm/yyyy");
 int dd=kb.nextInt();
 int mm=kb.nextInt();
 int yyyy=kb.nextInt();

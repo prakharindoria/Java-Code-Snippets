053e92d6cca267c7f27b490c0d3f00032dc16574
Prakhar Indoria <prakharindoria@gmail.com>
2020-11-04 22:59:35 +0530

    +HibernateApplication

---
+++ JEE_Snippets/JDBC Snippets/HibernateHelloWorld/src/in/scabhopal/hibernate/Employee.java
@@ -0,0 +1,29 @@
+package in.scabhopal.hibernate;
+
+public class Employee {
+	private int empNo;
+	private String empName;
+	private double empSal;
+	
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getEmpName() {
+		return empName;
+	}
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+	public double getEmpSal() {
+		return empSal;
+	}
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
+	}
+	
+	
+	
+}
---
+++ JEE_Snippets/JDBC Snippets/HibernateHelloWorld/src/in/scabhopal/hibernate/UseEmployee.java
@@ -0,0 +1,55 @@
+package in.scabhopal.hibernate;
+
+import java.util.Scanner;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UseEmployee {
+public static void main(String[] args) {
+	 
+		Configuration cfg=new Configuration();
+		cfg.configure();
+	    SessionFactory sf=cfg.buildSessionFactory();
+	    Session sess=sf.openSession();
+	   
+	   
+	   //Session sess=new Configuration().configure().buildSessionFactory().openSession();
+	   
+	   
+	   Scanner kb=new Scanner(System.in);
+	   System.out.println("Enter emp no:");
+	   int eno=kb.nextInt();
+	   
+	   
+	   Employee e=(Employee)sess.load(in.scabhopal.hibernate.Employee.class,eno);
+	   if(e!=null) {
+		   System.out.println("EmpNo:"+e.getEmpNo());
+		   System.out.println("Ename:"+e.getEmpName());
+		   System.out.println("Sal:"+e.getEmpSal());
+	   }
+	   else {
+		   System.out.println("Record not found");
+	   }
+	   
+	   
+	   //From Cache
+//	   System.out.println("Calling get second time");
+//	   e=(Employee)sess.get(in.scabhopal.hibernate.Employee.class,eno);
+//	   if(e!=null) {
+//		   System.out.println("EmpNo:"+e.getEmpNo());
+//		   System.out.println("Ename:"+e.getEmpName());
+//		   System.out.println("Sal:"+e.getEmpSal());
+//	   }
+//	   else {
+//		   System.out.println("Record not found");
+//	   }
+	   
+	   sess.close();
+	   sf.close();
+	   
+	   
+	
+}
+}
